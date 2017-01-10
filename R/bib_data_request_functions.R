ccc4c9185417d1dc663a770e9ec5034e7e3699cb
Dan Mason <dan@masonmob.com>
2017-01-10 21:49:18 +0000

    Added <AllowHidden> tags

--- R/bib_data_request_functions.R
+++ R/bib_data_request_functions.R
@@ -3,7 +3,7 @@
 # returns character containing XML string
 bibloadr_request_xml <- function(namelist = character(0), nametype = "variable",
                              level = character(0), cbtype = character(0), subclist = character(0),
-                             allow_null_ids = F, log = F, testmode = F) {
+                             allow_null_ids = F, allow_hidden = F, log = F, testmode = F) {
     
     # end of line for xml building
     eol <- "\n"
@@ -24,6 +24,7 @@ bibloadr_request_xml <- function(namelist = character(0), nametype = "variable",
     el_CB <- ifelse(length(cbtype)>0, paste0("<CodeBook>",cbtype,"</CodeBook>",eol), "")
     el_ML <- ifelse(length(level)>0, paste0("<MeasurementLevel>",level,"</MeasurementLevel>",eol), "")
     el_AN <- ifelse(allow_null_ids, paste0("<AllowNullIds>On</AllowNullIds>",eol), "")
+    al_AH <- ifelse(allow_hidden, paste0("<AllowHidden>On</AllowHidden>",eol), "")
     if (log) {
         el_LG <- paste0("<LogRequest>On</LogRequest>",eol)
         el_UN <- paste0("<User>","BiBUser","</User>",eol)
@@ -41,7 +42,7 @@ bibloadr_request_xml <- function(namelist = character(0), nametype = "variable",
     
     # build string vector of xml code and formatting elements
     # first, header up to list of names
-    xmlv <- paste0(el_DR[1], eol, el_CB, el_ML, el_AN, el_LG, el_UN, el_TM, el_SC, el_Nlist[1], eol)
+    xmlv <- paste0(el_DR[1], eol, el_CB, el_ML, el_AN, el_AH, el_LG, el_UN, el_TM, el_SC, el_Nlist[1], eol)
     # now output name list
     namelist <- paste0(el_Nitem[1], eol, el_Nattr[1], namelist, el_Nattr[2], 
                        eol, el_Nitem[2], eol)
@@ -153,7 +154,8 @@ label_data <- function (dat, var_labels, val_labels,
 # takes data request parameters and submits to bibloadr db, returning data frame
 # concatenates variables in varlist character vector to variables in varfile
 get_bibloadr_data <- function(varfile = character(0), varlist = character(0), level = character(0),
-                              allow_null_ids = F, label = T, log = F, testmode = F, devmode = F) {
+                              allow_null_ids = F, allow_hidden = F, label = T, log = F, testmode = F, 
+                              devmode = F) {
   
   # concatenate varlist vars to varfile vars
   varlist <- make_namelist(varfile, varlist)
@@ -165,7 +167,7 @@ get_bibloadr_data <- function(varfile = character(0), varlist = character(0), le
   sql_start <- "EXEC [ResearchMeta].[Explorer].[GetVariableData]\n@DataRequest = N'"
   
   sql_xml <- bibloadr_request_xml(namelist = varlist, level = level, allow_null_ids = allow_null_ids, 
-                                  log = log, testmode = testmode)
+                                  allow_hidden = allow_hidden, log = log, testmode = testmode)
   
   sql_end <- "';\n"
   
