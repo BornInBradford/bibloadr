4610ca3b92744c2c1527ad0fa75bd20e0182d978
Dan Mason <dan@masonmob.com>
2017-01-12 22:36:03 +0000

    test markdown doc now displays some basic variable info for a data frame. Needs to be rendered from the console to keep variables in scope, but this might be down to package versions.

---
+++ R/mddoc/render_test_rmd.R
@@ -0,0 +1,6 @@
+# render test.rmd
+# need to do this manually otherwise scope of variables seems limited to certain chunks
+# rendering from console gets around this
+
+rmarkdown::render("C:/repos/bibloadr/R/mddoc/test.rmd")
+
