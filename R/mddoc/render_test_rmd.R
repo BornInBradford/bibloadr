44db2acf63b0add86a2c8f41265d8909270f5211
Dan Mason <dan@masonmob.com>
2017-02-14 17:50:11 +0000

    Now does nice tables for pdf if using pander but can't get it to keep line breaks in description cell. Will keep line breaks if output in ascii using pandoc.table.

--- R/mddoc/render_test_rmd.R
+++ R/mddoc/render_test_rmd.R
@@ -2,5 +2,5 @@
 # need to do this manually otherwise scope of variables seems limited to certain chunks
 # rendering from console gets around this
 
-rmarkdown::render("C:/repos/bibloadr/R/mddoc/test2.rmd")
+rmarkdown::render("C:/repos/bibloadr/R/mddoc/test_pdf.rmd")
 
